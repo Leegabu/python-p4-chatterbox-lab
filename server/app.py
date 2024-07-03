e7016ae0679b813b897002f5cf9777139f74f8bf
Lee <leeirungu11313@gmail.com>
2024-07-03 21:36:32 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,37 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
+        return jsonify([{"id": message.id, "body": message.body, "username": message.username} for message in messages])
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(body=data["body"], username=data["username"])
+        db.session.add(message)
+        db.session.commit()
+        return jsonify({"id": message.id, "body": message.body, "username": message.username})
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if message is None:
+        return jsonify({"error": " Messages is not in the database"}), 404
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        message.body = data.get("body", message.body)
+        message.username = data.get("username", message.username)
+        db.session.add(message)
+        db.session.commit()
+        return jsonify({"id": message.id, "body": message.body, "username": message.username}), 200
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+    return jsonify({'deleted': True}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
